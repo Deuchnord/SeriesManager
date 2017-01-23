47884290f66231dd40e1739cdcdb80f8b5c21a08
Jérôme Deuchnord <astrofacile@gmail.com>
2017-01-23 20:33:26 +0100

    Ajout sécurité, création formulaire de login

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends Controller
+{
+	/**
+	 * @Route("/login", name="login")
+	 */
+	public function loginAction(Request $request)
+	{
+		$authenticationUtils = $this->get('security.authentication_utils');
+		$error = $authenticationUtils->getLastAuthenticationError();
+		$lastUsername = $authenticationUtils->getLastUsername();
+		
+		return $this->render('security/login.html.twig', [
+				'last_username' => $lastUsername,
+				'error' => $error
+		]);
+	}
+}
